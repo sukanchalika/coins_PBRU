0e111532158d2b9a20e96783b2c9cbbe0222b2ea
Sukan <ajsukan@gmail.com>
2015-08-20 13:48:33 +0700

    start app

---
+++ android/src/app/mygdx/music/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package app.mygdx.music.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import app.mygdx.music.MyGdxMusic;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxMusic(), config);
+	}
+}
